c3ade50398e24e81170f33b59ed525a053de6529
PRAGALANATHAN A <pragalanathan.eng@gmail.com>
2025-07-25 10:13:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("welcome to Junction");
+
+let FirstName="pragalanathan"
+let SecondName=" A"
+
+let FullName=FirstName+SecondName
+console.log(FullName)
+
+let BirthDay=("15/8/2003")
+console.log("My Birthday is "+BirthDay)
+
+let num1=15
+let num2=8
+let num3=2003
+
+let sum=num1+num2+num3;
+console.log("The sum is" +sum);
