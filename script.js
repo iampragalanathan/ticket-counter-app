6e5ddc3b496903a8a56237a385c11ddc18c8e4d1
PRAGALANATHAN A <pragalanathan.eng@gmail.com>
2025-07-25 10:49:33 +0530

    kjb

--- script.js
+++ script.js
@@ -9,12 +9,16 @@ console.log(FullName)
 let BirthDay=("15/8/2003")
 console.log("My Birthday is "+BirthDay)
 
+// =================================
+
 let num1=15
 let num2=8
 let num3=2003
 
 let sum=num1+num2+num3;
 console.log("The sum is" +sum)
+
+// ===================================
 
 let num4=15
 let num5=8
@@ -23,9 +27,18 @@ let num6=2003
 let joined=num4+num5+num6
 console.log(joined)
 
+// ====================================
+
 let add1=10
 let add2=5
 
 let add="10"-add2
 console.log(add)
 
+// ====================================
+
+let score=0
+console.log(score)
+
+score=3
+console.log(score)
