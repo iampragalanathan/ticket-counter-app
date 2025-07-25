cddacb4152455d1095b09542cd3ebfac7c938dd8
PRAGALANATHAN A <pragalanathan.eng@gmail.com>
2025-07-25 11:01:01 +0530

    drg

---
+++ basic.js
@@ -0,0 +1,47 @@
+
+
+
+console.log("welcome to Junction");
+
+let FirstName="pragalanathan"
+let SecondName=" A"
+
+let FullName=FirstName+SecondName
+console.log(FullName)
+
+let BirthDay=("15/8/2003")
+console.log("My Birthday is "+BirthDay)
+
+// =================================
+
+let num1=15
+let num2=8
+let num3=2003
+
+let sum=num1+num2+num3;
+console.log("The sum is" +sum)
+
+// ===================================
+
+let num4=15
+let num5=8
+let num6=2003
+
+let joined=num4+num5+num6
+console.log(joined)
+
+// ====================================
+
+let add1=10
+let add2=5
+
+let add="10"-add2
+console.log(add)
+
+// ====================================
+
+let score=0
+console.log(score)
+
+score=3
+console.log(score)
